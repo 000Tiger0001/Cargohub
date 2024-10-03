a4c93a3dc768682bd3ea98e60809c0fdf78caaf6
000Tiger0001 <daanvdmeer11@gmail.com>
2024-10-03 14:16:19 +0200

    tests for add transfer and update transfer complete

--- CargoHub/CargoHub/api/tests/test_transfers.py
+++ CargoHub/CargoHub/api/tests/test_transfers.py
@@ -59,7 +59,117 @@ class test_transfers(unittest.TestCase):
         self.assertEqual(self.transfers.get_transfers(), [])
     
     def test_add_transfer_bad(self) -> None:
-        ...
+        test_client = {
+        "id": 1,
+        "name": "Raymond Inc",
+        "address": "1296 Daniel Road Apt. 349",
+        "city": "Pierceview",
+        "zip_code": "28301",
+        "province": "Colorado",
+        "country": "United States",
+        "contact_name": "Bryan Clark",
+        "contact_phone": "242.732.3483x2573",
+        "contact_email": "robertcharles@example.net",
+        "created_at": "2010-04-28 02:22:53",
+        "updated_at": "2022-02-09 20:22:35"
+        }
+        self.assertEqual(self.transfers.get_transfers(), [])
+        self.transfers.add_transfer(test_client)
+        transfers = self.transfers.get_transfers().copy()
+        self.transfers.remove_transfer(1)
+        self.assertEqual(transfers, [])
+        self.assertEqual(self.transfers.get_transfers(), [])
+        
+    def test_add_transfer_with_duplicate_id(self) -> None:
+        test_transfer = {
+        "id": 2,
+        "reference": "TR00007",
+        "transfer_from": 9252,
+        "transfer_to": 9322,
+        "transfer_status": "Completed",
+        "created_at": "2000-10-23T01:52:14Z",
+        "updated_at": "2000-10-24T02:52:14Z",
+        "items": [
+            {
+                "item_id": "P002084",
+                "amount": 33
+            }
+        ]
+        }
+        self.transfers.add_transfer(self.test_transfer)
+        self.assertEqual(self.transfers.get_transfers(), [self.test_transfer])
+        self.transfers.add_transfer(test_transfer)
+        transfers = self.transfers.get_transfers().copy()
+        self.transfers.remove_transfer(2)
+        self.transfers.remove_transfer(2)
+        self.assertEqual(transfers, [self.test_transfer])
+        self.assertEqual(self.transfers.get_transfers(), [])
+    
+    def test_update_transfer(self) -> None:
+        test_updated_transfer = {
+        "id": 2,
+        "reference": "TR00007",
+        "transfer_from": 9252,
+        "transfer_to": 9322,
+        "transfer_status": "Completed",
+        "created_at": "2000-10-23T01:52:14Z",
+        "updated_at": "2000-10-24T02:52:14Z",
+        "items": [
+            {
+                "item_id": "P002084",
+                "amount": 33
+            }
+        ]
+        }
+        self.transfers.add_transfer(self.test_transfer)
+        self.assertEqual(self.transfers.get_transfer(2), self.test_transfer)
+        self.assertEqual(self.transfers.get_transfers(), [self.test_transfer])
+        self.transfers.update_transfer(2, test_updated_transfer)
+        self.assertEqual(self.transfers.get_transfer(2), test_updated_transfer)
+        self.assertNotEqual(self.test_transfer, test_updated_transfer)
+        self.transfers.remove_transfer(2)
+    
+    def test_update_transfer_with_duplicate_id(self) -> None:
+        test_transfer = {
+        "id": 7,
+        "reference": "TR00007",
+        "transfer_from": 9252,
+        "transfer_to": 9322,
+        "transfer_status": "Completed",
+        "created_at": "2000-10-23T01:52:14Z",
+        "updated_at": "2000-10-24T02:52:14Z",
+        "items": [
+            {
+                "item_id": "P002084",
+                "amount": 33
+            }
+        ]
+        }
+        test_updated_transfer = {
+        "id": 2,
+        "reference": "TR00007",
+        "transfer_from": 9252,
+        "transfer_to": 9322,
+        "transfer_status": "Completed",
+        "created_at": "2000-10-23T01:52:14Z",
+        "updated_at": "2000-10-24T02:52:14Z",
+        "items": [
+            {
+                "item_id": "P002084",
+                "amount": 33
+            }
+        ]
+        }
+        self.transfers.add_transfer(self.test_transfer)
+        self.assertEqual(self.transfers.get_transfers(), [self.test_transfer])
+        self.transfers.add_transfer(test_transfer)
+        self.assertEqual(self.transfers.get_transfers(), [self.test_transfer, test_transfer])
+        self.transfers.update_transfer(7, test_updated_transfer)
+        transfers = self.transfers.get_transfers().copy()
+        self.transfers.remove_transfer(2)
+        self.transfers.remove_transfer(2)
+        self.assertEqual(transfers, [self.test_transfer, test_transfer])
+        self.assertEqual(self.transfers.get_transfers(), [])
 
 if __name__ == '__main__':
     unittest.main()
