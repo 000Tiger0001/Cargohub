467c61612f99c5743db43fecdc80b731f1adcfda
Daan van der Meer <daanvdmeer11@gmail.com>
2024-11-13 11:21:43 +0100

    finished warehouse controllers

--- Cargo/api/controllers/WarehouseControllers.cs
+++ Cargo/api/controllers/WarehouseControllers.cs
@@ -3,5 +3,54 @@ using Microsoft.AspNetCore.Mvc;
 [Route("Cargohub")]
 public class WarehouseControllers : Controller
 {
+    WarehouseServices WS;
 
+    public WarehouseControllers(WarehouseServices ws)
+    {
+        WS = ws;
+    }
+
+    [HttpGet("/warehouses")]
+    public async Task<IActionResult> GetWarehouses()
+    {
+        List<Warehouse> warehouses = await WS.GetWarehouses();
+        return Ok(warehouses);
+    }
+
+    [HttpGet("/warehouse")]
+    public async Task<IActionResult> GetWarehouse([FromQuery] Guid warehouseId)
+    {
+        Warehouse warehouse = await WS.GetWarehouse(warehouseId);
+        if (warehouse is not null) return Ok(warehouse);
+        return BadRequest("No warehouse found with this id. ");
+    }
+
+    [HttpPost("/add-warehouse")]
+    public async Task<IActionResult> AddWarehouse([FromBody] Warehouse warehouse)
+    {
+        if (warehouse is null || warehouse.Code == default(string) || warehouse.Name == default(string) || warehouse.Address == default(string) || warehouse.Zip == default(string) || warehouse.City == default(string) || warehouse.Province == default(string) || warehouse.Country == default(string)) return BadRequest("Not enough info given");
+        warehouse.Id = Guid.NewGuid();
+
+        bool IsAdded = await WS.AddWarehouse(warehouse);
+        if (!IsAdded) return BadRequest("This warehouse cannot be added. ");
+        return Ok("Warehouse added. ");
+    }
+
+    public async Task<IActionResult> UpdateWarehouse([FromBody] Warehouse warehouse)
+    {
+        if (warehouse.Id == Guid.Empty) return BadRequest("Warehouse doesn't have an id. ");
+
+        bool IsUpdated = await WS.UpdateWarehouse(warehouse);
+        if (!IsUpdated) return BadRequest("Warehouse couldn't be updated. ");
+        return Ok("Warehouse updated. ");
+    }
+
+    public async Task<IActionResult> RemoveWarehouse([FromQuery] Guid warehouseId)
+    {
+        if (warehouseId == Guid.Empty) return BadRequest("Cannot remove warehouse with empty id. ");
+
+        bool IsRemoved = await WS.RemoveWarehouse(warehouseId);
+        if (!IsRemoved) return BadRequest("Warehouse couldn't be removed. ");
+        return Ok("Warehouse removed. ");
+    }
 }
