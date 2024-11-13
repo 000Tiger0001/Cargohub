ff06583fc6d67a5c5a346cfc5a80a0425d1a7eb8
Anton Ishchyk <anton.ishchyk@gmail.com>
2024-11-13 09:54:02 +0100

    add entity frameword, add set models

---
+++ Cargo/api/Access/DbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    // Define DbSet properties for each entity
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Inventory> Inventories { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<ItemGroup> ItemGroups { get; set; }
+    public DbSet<ItemLine> ItemLines { get; set; }
+    public DbSet<ItemType> ItemTypes { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Shipment> Shipments { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+    public DbSet<Transfer> Transfers { get; set; }
+    public DbSet<Warehouse> Warehouses { get; set; }
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure entities and relationships here if needed
+    }
+}
