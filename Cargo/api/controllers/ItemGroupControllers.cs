2a69a570aff3f1cb6cacad9ffcbdc18be9e2f4aa
Daan van der Meer <daanvdmeer11@gmail.com>
2024-11-20 15:56:14 +0100

    fixed controllers and services of item groups

--- Cargo/api/controllers/ItemGroupControllers.cs
+++ Cargo/api/controllers/ItemGroupControllers.cs
@@ -3,21 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 [Route("Cargohub")]
 public class ItemGroupControllers : Controller
 {
-    ItemGroupServices IGS;
+    private ItemGroupServices _itemGroupService;
 
-    public ItemGroupControllers(ItemGroupServices igs)
+    public ItemGroupControllers(ItemGroupServices itemGroupService)
     {
-        IGS = igs;
+        _itemGroupService = itemGroupService;
     }
 
     [HttpGet("get-item-groups")]
-    public async Task<IActionResult> GetItemGroups() => Ok(await IGS.GetItemGroups());
+    public async Task<IActionResult> GetItemGroups() => Ok(await _itemGroupService.GetItemGroups());
 
     [HttpGet("get-item-group")]
-    public async Task<IActionResult> GetItemGroup([FromQuery] Guid itemGroupId)
+    public async Task<IActionResult> GetItemGroup([FromQuery] int itemGroupId)
     {
-        if (itemGroupId == Guid.Empty) return BadRequest("You can't use an empty string. ");
-        ItemGroup itemGroup = await IGS.GetItemGroup(itemGroupId);
+        if (itemGroupId == 0) return BadRequest("You can't use an empty string. ");
+        ItemGroup? itemGroup = await _itemGroupService.GetItemGroup(itemGroupId);
         if (itemGroup is null) return BadRequest("Item group not found. ");
         return Ok(itemGroup);
     }
@@ -27,7 +27,7 @@ public class ItemGroupControllers : Controller
     {
         if (itemGroup is null || itemGroup.Name == "") return BadRequest("Not enough info. ");
 
-        bool IsAdded = await IGS.AddItemGroup(itemGroup);
+        bool IsAdded = await _itemGroupService.AddItemGroup(itemGroup);
         if (!IsAdded) return BadRequest("Can't add item group. ");
         return Ok("Item group added. ");
     }
@@ -35,19 +35,19 @@ public class ItemGroupControllers : Controller
     [HttpPut("update-item-group")]
     public async Task<IActionResult> UpdateItemGroup([FromBody] ItemGroup itemGroup)
     {
-        if (itemGroup is null || itemGroup.Id == Guid.Empty) return BadRequest("Not enough info. ");
+        if (itemGroup is null || itemGroup.Id == 0) return BadRequest("Not enough info. ");
         
-        bool IsUpdated = await IGS.UpdateItemGroup(itemGroup);
+        bool IsUpdated = await _itemGroupService.UpdateItemGroup(itemGroup);
         if(!IsUpdated) return BadRequest("Item group can't be updated. ");
         return Ok("Item group updated. ");
     }
 
     [HttpDelete("remove-item-group")]
-    public async Task<IActionResult> RemoveItemGroup([FromQuery] Guid itemGroupId)
+    public async Task<IActionResult> RemoveItemGroup([FromQuery] int itemGroupId)
     {
-        if (itemGroupId == Guid.Empty) return BadRequest("Can't remove item group with empty id. ");
+        if (itemGroupId == 0) return BadRequest("Can't remove item group with empty id. ");
 
-        bool IsRemoved = await IGS.RemoveItemGroup(itemGroupId);
+        bool IsRemoved = await _itemGroupService.RemoveItemGroup(itemGroupId);
         if (!IsRemoved) return BadRequest("Couldn't remove item group with given id. ");
         return Ok("Item group removed. ");
     }
--- Cargo/api/services/ItemGroupServices.cs
+++ Cargo/api/services/ItemGroupServices.cs
@@ -1,43 +1,30 @@
 public class ItemGroupServices
 {
-    public async Task<List<ItemGroup>> GetItemGroups() => await AccessJson.ReadJson<ItemGroup>();
+    private ItemGroupAccess _itemGroupAccess;
 
-    public async Task<ItemGroup> GetItemGroup(Guid itemGroupId)
+    public ItemGroupServices(ItemGroupAccess itemGroupAccess)
     {
-        List<ItemGroup> itemGroups = await GetItemGroups();
-        return itemGroups.FirstOrDefault(i => i.Id == itemGroupId)!;
+        _itemGroupAccess = itemGroupAccess;
     }
+
+    public async Task<List<ItemGroup>> GetItemGroups() => await _itemGroupAccess.GetAll();
+
+    public async Task<ItemGroup?> GetItemGroup(int itemGroupId) => await _itemGroupAccess.GetById(itemGroupId);
 
     public async Task<bool> AddItemGroup(ItemGroup itemGroup)
     {
         if (itemGroup is null || itemGroup.Name == "") return false;
         List<ItemGroup> itemGroups = await GetItemGroups();
         ItemGroup doubleItemGroup = itemGroups.Find(i => i.Name == itemGroup.Name)!;
-        if (doubleItemGroup is null) return false;
-        await AccessJson.WriteJson(itemGroup);
-        return true;
+        return await _itemGroupAccess.Add(itemGroup);
     }
 
     public async Task<bool> UpdateItemGroup(ItemGroup itemGroup)
     {
-        if (itemGroup is null) return false;
-        List<ItemGroup> itemGroups = await GetItemGroups();
-        int itemGroupIndex = itemGroups.FindIndex(i => i.Id == itemGroup.Id);
-        if (itemGroupIndex == -1) return false;
+        if (itemGroup is null || itemGroup.Id == 0) return false;
         itemGroup.UpdatedAt = DateTime.Now;
-        itemGroups[itemGroupIndex] = itemGroup;
-        AccessJson.WriteJsonList(itemGroups);
-        return true;
+        return await _itemGroupAccess.Update(itemGroup);
     }
 
-    public async Task<bool> RemoveItemGroup(Guid itemGroupId)
-    {
-        if (itemGroupId == Guid.Empty) return false;
-        List<ItemGroup> itemGroups = await GetItemGroups();
-        ItemGroup foundItemGroup = itemGroups.FirstOrDefault(i => i.Id == itemGroupId)!;
-        if (foundItemGroup is null) return false;
-        itemGroups.Remove(foundItemGroup);
-        AccessJson.WriteJsonList(itemGroups);
-        return true;
-    }
+    public async Task<bool> RemoveItemGroup(int itemGroupId) => await _itemGroupAccess.Remove(itemGroupId);
 }
