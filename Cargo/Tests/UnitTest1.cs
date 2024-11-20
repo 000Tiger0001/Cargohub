4ee35f24612dc0f61d2d14aa94479dd655e85527
Anton Ishchyk <anton.ishchyk@gmail.com>
2024-11-20 13:18:58 +0100

    Connect tests with project

---
+++ Cargo/Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Tests;
+using api;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
