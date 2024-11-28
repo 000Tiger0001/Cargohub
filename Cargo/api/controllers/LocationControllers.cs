40fd9bd19db62eb8df3950c0c9a066cb6de50f9b
Anton Ishchyk <anton.ishchyk@gmail.com>
2024-11-28 19:35:13 +0100

    locations

--- Cargo/api/controllers/LocationControllers.cs
+++ Cargo/api/controllers/LocationControllers.cs
@@ -17,23 +17,23 @@ public class LocationControllers : Controller
         return Ok(locations);
     }
 
-    [HttpGet("location")]
-    public async Task<IActionResult> GetLocation([FromQuery] int locationId)
+    [HttpGet("location/{locationId}")]
+    public async Task<IActionResult> GetLocation(int locationId)
     {
         Location? location = await _locationAccess.GetLocation(locationId);
         if (location is not null) return Ok(location);
         return BadRequest("There is no location with the given id. ");
     }
 
-    [HttpGet("locations-from-warehouse")]
-    public async Task<IActionResult> GetLocationsFromWarehouse([FromQuery] int warehouseId)
+    [HttpGet("warehouse/{warehouseId}/locations")]
+    public async Task<IActionResult> GetLocationsFromWarehouse(int warehouseId)
     {
         List<Location> locations = await _locationAccess.GetLocationsInWarehouse(warehouseId);
         if (locations.Count > 0) return Ok(locations);
         return BadRequest("There are no locations with the given warehouse id. ");
     }
 
-    [HttpPost("add-location")]
+    [HttpPost("location")]
     public async Task<IActionResult> AddLocation([FromBody] Location location)
     {
         if (location.WarehouseId == 0 || location.Id == 0 || location.Code == default || location.Name == default) BadRequest("Data incomplete. ");
@@ -43,7 +43,7 @@ public class LocationControllers : Controller
         return Ok("Location added. ");
     }
 
-    [HttpPut("update-location")]
+    [HttpPut("location")]
     public async Task<IActionResult> UpdateLocation([FromBody] Location location)
     {
         if (location.Id == 0) return BadRequest("Location doesn't have an id. ");
@@ -53,7 +53,7 @@ public class LocationControllers : Controller
         return Ok("Location updated. ");
     }
 
-    [HttpDelete("remove-location")]
+    [HttpDelete("location")]
     public async Task<IActionResult> RemoveLocation([FromQuery] int locationId)
     {
         if (locationId == 0) return BadRequest("Cannot remove location with empty id. ");
