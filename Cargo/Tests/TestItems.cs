33efeccf224554b3e1ee18369650d39ace29ac91
Daan van der Meer <daanvdmeer11@gmail.com>
2024-11-28 09:45:54 +0100

    test GetAllItems

---
+++ Cargo/Tests/TestItems.cs
@@ -0,0 +1,34 @@
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+
+public class ItemTests
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly ItemAccess _itemAccess;
+    private readonly ItemServices _service;
+
+    public ItemTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                        .UseInMemoryDatabase(Guid.NewGuid().ToString()) // In-memory database
+                        .Options;
+        _dbContext = new ApplicationDbContext(options);
+        _itemAccess = new ItemAccess(_dbContext);
+        _service = new(_itemAccess);
+    }
+
+    public async Task GetAllItems()
+    {
+        Item mockItem = new(1, "sjQ23408K", "Face-to-face clear-thinking complexity", "must", "6523540947122", "63-OFFTq0T", "oTo304", 11, 73, 14, 47, 13, 11, 34, "SUP423", "E-86805-uTM");
+
+        Assert.Empty(await _service.GetItems());
+
+        await _service.AddItem(mockItem);
+
+        Assert.Equal([mockItem], await _service.GetItems());
+
+        await _service.RemoveItem(1);
+
+        Assert.Empty(await _service.GetItems());
+    }
+}
