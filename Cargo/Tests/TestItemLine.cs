53d88faea84d1f7d4060fdf4cb6c028a7806a23e
Daan van der Meer <daanvdmeer11@gmail.com>
2024-11-28 11:57:23 +0100

    test GetAllItemLines

--- Cargo/Tests/TestItemLine.cs
+++ Cargo/Tests/TestItemLine.cs
@@ -16,4 +16,20 @@ public class ItemLineTests
         _itemLineAccess = new ItemLineAccess(_dbContext);
         _service = new(_itemLineAccess);
     }
+
+    [Fact]
+    public async Task GetAllItemLines()
+    {
+        ItemLine mockItemLine = new(1, "Home Appliances", "Stuff for home");
+
+        Assert.Empty(await _service.GetItemLines());
+
+        await _service.AddItemLine(mockItemLine);
+
+        Assert.Equal([mockItemLine], await _service.GetItemLines());
+
+        await _service.RemoveItemLine(1);
+
+        Assert.Empty(await _service.GetItemLines());
+    }
 }
