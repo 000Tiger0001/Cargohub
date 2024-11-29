8a4de5b6aa490da3faff258bbb3ab1133ec41d4d
Daan van der Meer <daanvdmeer11@gmail.com>
2024-11-29 09:25:14 +0100

    test AddDuplicateLocation

--- Cargo/Tests/TestLocations.cs
+++ Cargo/Tests/TestLocations.cs
@@ -125,4 +125,25 @@ public class LocationTests
 
         Assert.Empty(await _locationService.GetLocations());
     }
+
+    [Fact]
+    public async Task AddDuplicateLocation()
+    {
+        Location mockLocation1 = new(1, 1, "A.1.0", "Row: A, Rack: 1, Shelf: 0");
+
+        bool IsAdded = await _locationService.AddLocation(mockLocation1);
+
+        Assert.True(IsAdded);
+        Assert.Equal([mockLocation1], await _locationService.GetLocations());
+
+        bool IsAdded1 = await _locationService.AddLocation(mockLocation1);
+
+        Assert.False(IsAdded1);
+        Assert.Equal([mockLocation1], await _locationService.GetLocations());
+
+        bool IsRemoved = await _locationService.RemoveLocation(1);
+
+        Assert.True(IsRemoved);
+        Assert.Empty(await _locationService.GetLocations());
+    }
 }
