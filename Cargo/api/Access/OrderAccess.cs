e3b3c2955ad6a2365212544e45b2b1702dab9c87
Anton Ishchyk <anton.ishchyk@gmail.com>
2024-12-04 10:15:43 +0100

    override getall, getbyid for orders. update to be made

--- Cargo/api/Access/OrderAccess.cs
+++ Cargo/api/Access/OrderAccess.cs
@@ -1,4 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
 public class OrderAccess : BaseAccess<Order>
 {
     public OrderAccess(ApplicationDbContext context) : base(context) { }
+
+    public override async Task<List<Order>> GetAll()
+    {
+        List<Order> orders = await _context.Set<Order>().AsNoTracking()
+            .Include(order => order.Items) // Include OrderItemMovement entities
+            .ToListAsync();
+        return orders;
+    }
+
+    public override async Task<Order?> GetById(int orderId)
+    {
+        Order? order = await _context.Set<Order>().AsNoTracking()
+            .Include(o => o.Items)  // Explicitly include related OrderItemMovements
+            .FirstOrDefaultAsync(o => o.Id == orderId);
+
+        return order;
+    }
+
+    public override async Task<bool> Update(Order order)
+    {
+        return true;
+    }
 }
