90fcaa65efadbd27e2812317dfc22acb417b10f6
Anton Ishchyk <anton.ishchyk@gmail.com>
2024-12-04 12:57:12 +0100

    get all shipments, get shipment by id

--- Cargo/api/Access/ShipmentAccess.cs
+++ Cargo/api/Access/ShipmentAccess.cs
@@ -1,4 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
 public class ShipmentAccess : BaseAccess<Shipment>
 {
     public ShipmentAccess(ApplicationDbContext context) : base(context) { }
+
+    public override async Task<List<Shipment>> GetAll()
+    {
+        List<Shipment> shipments = await _context.Set<Shipment>().AsNoTracking()
+            .Include(shipment => shipment.Items)
+            .ToListAsync();
+        return shipments;
+    }
+
+    public override async Task<Shipment?> GetById(int shipmentId)
+    {
+        Shipment? shipment = await _context.Set<Shipment>().AsNoTracking()
+            .Include(s => s.Items)
+            .FirstOrDefaultAsync(s => s.Id == shipmentId);
+        return shipment;
+    }
 }
