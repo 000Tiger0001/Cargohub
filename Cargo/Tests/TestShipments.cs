02541d7cb6c06fc6fb0b7e28b9e4533ee1494d5f
Daan van der Meer <daanvdmeer11@gmail.com>
2024-12-04 09:01:14 +0100

    initialized ShipmentTests

---
+++ Cargo/Tests/TestShipments.cs
@@ -0,0 +1,19 @@
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+
+public class ShipmentTests
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly ShipmentAccess _shipmentAccess;
+    private readonly ShipmentServices _service;
+
+    public ShipmentTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                        .UseInMemoryDatabase(Guid.NewGuid().ToString()) // In-memory database
+                        .Options;
+        _dbContext = new ApplicationDbContext(options);
+        _shipmentAccess = new ShipmentAccess(_dbContext);
+        _service = new(_shipmentAccess);
+    }
+}
