b019abc6b0eba24cfa63063e4aed77d0fffb40ff
Daan van der Meer <daanvdmeer11@gmail.com>
2024-12-04 15:07:33 +0100

    test AddSupplierWithDuplicateId

--- Cargo/Tests/TestSuppliers.cs
+++ Cargo/Tests/TestSuppliers.cs
@@ -92,4 +92,26 @@ public class SupplierTests
         Assert.True(IsRemoved);
         Assert.Empty(await _service.GetSuppliers());
     }
+
+    [Fact]
+    public async Task AddSupplierWithDuplicateId()
+    {
+        Supplier mockSupplier1 = new(1, "SUP0001", "Lee, Parks and Johnson", "5989 Sullivan Drives", "Apt. 996", "Port Anitaburgh", "91688", "Illinois", "Czech Republic", "Toni Barnett", "363.541.7282x36825", "LPaJ-SUP0001");
+        Supplier mockSupplier2 = new(1, "SUP0002", "Holden-Quinn", "576 Christopher Roads", "Suite 072", "Amberbury", "16105", "Illinois", "Saint Martin", "Kathleen Vincent", "001-733-291-8848x3542", "H-SUP0002");
+
+        bool IsAdded1 = await _service.AddSupplier(mockSupplier1);
+
+        Assert.True(IsAdded1);
+        Assert.Equal([mockSupplier1], await _service.GetSuppliers());
+
+        bool IsAdded2 = await _service.AddSupplier(mockSupplier2);
+
+        Assert.False(IsAdded2);
+        Assert.Equal([mockSupplier1], await _service.GetSuppliers());
+
+        bool IsRemoved = await _service.RemoveSupplier(1);
+
+        Assert.True(IsRemoved);
+        Assert.Empty(await _service.GetSuppliers());
+    }
 }
