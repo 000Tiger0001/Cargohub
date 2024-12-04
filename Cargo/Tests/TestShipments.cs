f0137c0e98ab23b192407566edd04d5d45169638
Daan van der Meer <daanvdmeer11@gmail.com>
2024-12-04 10:00:07 +0100

    test AddShipmentGood

--- Cargo/Tests/TestShipments.cs
+++ Cargo/Tests/TestShipments.cs
@@ -47,4 +47,20 @@ public class ShipmentTests
 
         Assert.Empty(await _service.GetShipments());
     }
+
+    [Fact]
+    public async Task AddShipmentGood()
+    {
+        Shipment mockShipment = new(1, 1, 33, DateTime.Parse("2000-03-09"), DateTime.Parse("2000-03-11"), DateTime.Parse("2000-03-13"), 'I', "Pending", "Zee vertrouwen klas rots heet lachen oneven begrijpen.", "DPD", "Dynamic Parcel Distribution", "Fastest", "Manual", "Ground", 18, 594.42, [new(7435, 23), new(9557, 1), new(9553, 50)]);
+
+        bool IsAdded = await _service.AddShipment(mockShipment);
+
+        Assert.True(IsAdded);
+        Assert.Equal([mockShipment], await _service.GetShipments());
+
+        bool IsRemoved = await _service.RemoveShipment(1);
+
+        Assert.True(IsRemoved);
+        Assert.Empty(await _service.GetShipments());
+    }
 }
