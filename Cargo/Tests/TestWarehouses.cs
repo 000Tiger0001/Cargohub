9dabc35bd32532088bd7281d383060fd90dab571
000Tiger0001 <daanvdmeer11@gmail.com>
2024-12-05 11:46:07 +0100

    test AddWarehouse

--- Cargo/Tests/TestWarehouses.cs
+++ Cargo/Tests/TestWarehouses.cs
@@ -53,4 +53,22 @@ public class WarehouseTests
 
         Assert.Null(await _service.GetWarehouse(1));
     }
+
+    [Fact]
+    public async Task AddWarehouse()
+    {
+        Warehouse mockWarehouse = new(1, "YQZZNL56", "Heemskerk cargo hub", "Karlijndreef 281", "4002 AS", "Heemskerk", "Friesland", "NL", "Fem Keijzer", "(078) 0013363", "blamore@example.net");
+
+        Assert.Empty(await _service.GetWarehouses());
+
+        bool IsAdded = await _service.AddWarehouse(mockWarehouse);
+
+        Assert.True(IsAdded);
+        Assert.Equal([mockWarehouse], await _service.GetWarehouses());
+
+        bool IsRemoved = await _service.RemoveWarehouse(1);
+
+        Assert.True(IsRemoved);
+        Assert.Empty(await _service.GetWarehouses());
+    }
 }
