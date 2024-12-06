6fc7e95d902c90e781238c928f1fbd2d66269ef8
000Tiger0001 <daanvdmeer11@gmail.com>
2024-12-06 13:47:02 +0100

    Create ShipmentItemMovementServices.cs

---
+++ Cargo/api/services/ShipmentItemMovementServices.cs
@@ -0,0 +1,9 @@
+public class ShipmentItemMovementServices
+{
+    private ShipmentItemMovementAccess _access;
+
+    public ShipmentItemMovementServices(ShipmentItemMovementAccess shipmentItemMovementAccess)
+    {
+        _access = shipmentItemMovementAccess;
+    }
+}
