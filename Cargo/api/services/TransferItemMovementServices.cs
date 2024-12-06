b35fef09c386ed351eb16f915ac152db5d9594b6
000Tiger0001 <daanvdmeer11@gmail.com>
2024-12-06 13:58:17 +0100

    Create TransferItemMovementServices.cs

---
+++ Cargo/api/services/TransferItemMovementServices.cs
@@ -0,0 +1,9 @@
+public class TransferItemMovementServices
+{
+    private TransferItemMovementAccess _access;
+
+    public TransferItemMovementServices(TransferItemMovementAccess transferItemMovementAccess)
+    {
+        _access = transferItemMovementAccess;
+    }
+}
