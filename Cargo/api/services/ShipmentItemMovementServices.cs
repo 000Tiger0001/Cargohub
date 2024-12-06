6cb1ca08acdb2bdeff370f7300aec7516e28fd0a
000Tiger0001 <daanvdmeer11@gmail.com>
2024-12-06 13:55:51 +0100

    CRUD for ShipmentItemMovement

--- Cargo/api/services/ShipmentItemMovementServices.cs
+++ Cargo/api/services/ShipmentItemMovementServices.cs
@@ -6,4 +6,24 @@ public class ShipmentItemMovementServices
     {
         _access = shipmentItemMovementAccess;
     }
+
+    public async Task<List<ShipmentItemMovement>> GetShipmentItemMovements() => await _access.GetAll();
+
+    public async Task<ShipmentItemMovement?> GetShipmentItemMovement(int shipmentItemMovementId) => await _access.GetById(shipmentItemMovementId);
+
+    public async Task<bool> AddShipmentItemMovement(ShipmentItemMovement shipmentItemMovement)
+    {
+        List<ShipmentItemMovement> shipmentItemMovements = await GetShipmentItemMovements();
+        ShipmentItemMovement doubleShipmentItemMovement = shipmentItemMovements.FirstOrDefault(s => s.Id == shipmentItemMovement.Id || (s.Amount == shipmentItemMovement.Amount && s.ItemId == shipmentItemMovement.ItemId))!;
+        if (doubleShipmentItemMovement is not null) return false;
+        return await _access.Add(shipmentItemMovement);
+    }
+
+    public async Task<bool> UpdateShipmentItemMovement(ShipmentItemMovement shipmentItemMovement)
+    {
+        if (shipmentItemMovement is null || shipmentItemMovement.Id == 0 || shipmentItemMovement.ItemId == 0 || shipmentItemMovement.Amount == 0) return false;
+        return await _access.Update(shipmentItemMovement);
+    }
+
+    public async Task<bool> RemoveShipmentItemMovement(int shipmentItemMovementId) => await _access.Remove(shipmentItemMovementId);
 }
