a2e9b5fb6439b59cfe520dc3ffd2da60e03b8f3c
Daan van der Meer <daanvdmeer11@gmail.com>
2024-12-11 09:51:37 +0100

    fixed AddItemWithoutItemGroup

--- Cargo/Tests/IntegrationTests.cs
+++ Cargo/Tests/IntegrationTests.cs
@@ -309,8 +309,13 @@ public class IntegrationTests
     [Fact]
     public async Task AddItemWithoutItemGroup()
     {
-        var testItem = new Item { Id = 1, ItemGroupId = 1 };
-        await _serviceItems.AddItem(testItem);
+        Item testItem = new(1, "sjQ23408K", "Face-to-face clear-thinking complexity", "must", "6523540947122", "63-OFFTq0T", "oTo304", 11, 1, 1, 47, 13, 11, 34, "SUP423", "E-86805-uTM"); ;
+
+        bool IsItemAdded = await _serviceItems.AddItem(testItem);
+
+        Assert.True(IsItemAdded);
+        Assert.Equal([testItem], await _serviceItems.GetItems());
+        
         Assert.Null(await _serviceItems.GetItem(1));
     }
 
